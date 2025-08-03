3b0bbe5e89ebde9f5157df41c8c6a68094a474fc
Nguyen Duy <duy10tinhvt@gmail.com>
2025-08-03 12:44:24 +0700

    first commit

---
+++ src/main/java/com/project/shopapp/Controllers/CategoryController.java
@@ -0,0 +1,51 @@
+package com.project.shopapp.Controllers;
+
+import com.project.shopapp.dtos.CategoryDTO;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/categories")
+//@Validatedl
+public class CategoryController {
+    // Hiển thị tất cả categories
+    @GetMapping("") //http://localhost:8088/api/v1/categories?page=1&limit=10
+    public ResponseEntity<String> getAllCategories(
+            @RequestParam("page")  int page,
+            @RequestParam("limit") int limit
+    ) {
+        return ResponseEntity.ok(String.format("getAllCategories, page = %d , limit= %d" , page, limit));
+    }
+
+    @PostMapping("")
+    // Nếu tham số là 1 object => Data Tranfer Object
+    public ResponseEntity<?> insertCategory(
+            @Valid @RequestBody CategoryDTO categoryDTO, BindingResult result){
+        if (result.hasErrors()) {
+            List<String> errorMessages = result.getFieldErrors()
+                    .stream()
+                    .map(FieldError::getDefaultMessage)
+                    .toList();
+
+            return ResponseEntity.badRequest().body(errorMessages);
+        }
+        return ResponseEntity.ok("Test thu thoi" + categoryDTO);
+    }
+// Valid kich hoat kiem tra rang buoc
+// @RequestBody de anh xa du lieu tu cline sang
+
+    @PutMapping ("/{id}")
+    public ResponseEntity<String> updateCategory(@PathVariable Long id){
+        return ResponseEntity.ok("Test thu lan 2" + id);
+    }
+    @DeleteMapping ("/{id}")
+    public ResponseEntity<String> deleteCategory(@PathVariable Long id){
+        return ResponseEntity.ok("Delete with " + id);
+    }
+
+}
---
+++ src/main/java/com/project/shopapp/dtos/CategoryDTO.java
@@ -0,0 +1,14 @@
+package com.project.shopapp.dtos;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.*;
+
+@Data // tostring
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class CategoryDTO {
+    @NotEmpty(message = "khong duoc de trong ten -  Category's name can not be empty ")
+    private  String name ;
+}
